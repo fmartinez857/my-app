38c3eb080777c30d1e1c9ab20df551954e863fa0
fernando <fmartinez@ciencias.unam.mx>
2021-12-15 21:17:06 -0600

    Version final del Proyecto my-app de prueba

---
+++ src/main/java/com/fernando/app/entity/TestEntity.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.fernando.app.entity;
+
+/**
+ * @author Fernando Martinez Z
+ *
+ */
+public class TestEntity {
+	
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo java
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
